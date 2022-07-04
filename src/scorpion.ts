39be774de03dc75b3396825e27e54f649e695af3
Caspar <caspar.bosch@gmail.com>
2022-07-05 01:23:45 +0200

    3

--- src/scorpion.ts
+++ src/scorpion.ts
@@ -21,50 +21,6 @@ export class Scorpion extends PIXI.Sprite {
         window.addEventListener("keyup", (e: KeyboardEvent) => this.onKeyUp(e));
     }
 
-    //keyboard input clickevent check voor WASD-keys
-    private onKeyDown(e: KeyboardEvent): void {
-        switch (e.key.toUpperCase()) {
-            case ".":
-                this.shoot()
-            case "ARROWLEFT":
-                this.xSpeed = -4
-                break
-            case "ARROWRIGHT":
-                this.xSpeed = 4
-                break
-                //ARROWUP is for jump
-            case "ARROWUP":
-                // this.ySpeed = -4
-                break
-                //ARROWDOWN is for duck
-            case "ARROWDOWN":
-                // this.ySpeed = 4
-                break
-        }
-
-    }
-
-    private onKeyUp(e: KeyboardEvent): void {
-        switch (e.key.toUpperCase()) {
-            case ".":
-                this.shoot()
-                break;
-            case "ARROWLEFT":
-            case "ARROWRIGHT":
-                this.xSpeed = 0
-                break
-            case "ARROWUP":
-            case "ARROWDOWN":
-                this.ySpeed = 0
-                break
-        }
-    }
-
-    public update(delta: number){
-        this.x += this.xSpeed * delta
-        this.y += this.ySpeed * delta
-    }
-
     private shoot(){
         this.game.addBlast(this.x + 80, this.y + 35)
     }
